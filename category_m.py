f93f89a99ab21ba0b1a3cdcd4e2fa6febbc09027
VladVasili4 <dipol777@ya.ru>
2024-10-18 07:37:11 +0300

    Add files via upload

--- category_m.py
+++ category_m.py
@@ -1,11 +1,19 @@
 from app.backend.db import Base
 from sqlalchemy import Integer, Column, String, Boolean, ForeignKey # ForeignKey - ключ для связывания таблиц
 from sqlalchemy import relationship
+from app.models import *
 
 
 class Category(Base):
-    __tablename__ = 'category'
+    __tablename__ = 'categories'
+    __table_args__ = {'extend_existing': True}
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     slug = Column(String, unique=True, index=True)
     is_active = Column(Boolean, default=True)
+
+    product = relationship('Product', back_populates='category')  #!!! 'category' а не 'categories'
+
+
+from sqlalchemy.schema import CreateTable
+print(CreateTable(Category.__table__))
--- products_m.py
+++ products_m.py
@@ -1,2 +1,19 @@
+from app.backend.db import Base
+from sqlalchemy import Integer, Column, String, Boolean, ForeignKey, Float # ForeignKey - ключ для связывания таблиц
+from sqlalchemy import relationship
 
+class Product(Base):  # Собрали таблицу. Есть специальный способ, но мы решили сделать это так. Что за способ? не сказал.
+    __tablename__ = 'products'
+    __table_args__ = {'keep_existing': True}
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    slug = Column(String, unique=True, index=True)
+    description = Column(String)
+    price = Column(Integer)     #   По хорошему, д.б. float, но лектор решил так
+    image_url = Column(String)
+    stock = Column(Integer)
+    rating = Column(Float)
+    is_active = Column(Boolean, default=True)
+    category_id = Column(Integer, ForeignKey='categories.id') # Обеспечивает связь один ко многим?
 
+    category = relationship('Category', back_populates='products')
